22090036b0de71980b4eb79e8364166b0ef74ae6
Joelle Plow <joelle@Joelles-MacBook-Pro-2.local>
2021-06-20 21:38:15 +0100

    add youtube videos for each key

--- src/components/CorrectAnswer.tsx
+++ src/components/CorrectAnswer.tsx
@@ -4,6 +4,21 @@ import {selectScaleEditor} from '../features/scaleEditor/scaleEditorSlice';
 import {useSynth} from '../features/synth/synth';
 import parsePitch, {pitchClassOf} from '../pitch';
 import PianoKeyboard from './PianoKeyboard';
+import YouTube from 'react-youtube'
+
+const youtubeVideos:{[key: string]: string} = {
+  "D Major":  "ngE4Xrs9IgA",
+  "G Major":  "ngE4Xrs9IgA",
+  "C Major":  "Rk72m9bLrBo",
+  "A Major":  "Xdx4fVxceuQ",
+  "E Major":  "bfOOWtdqZIg",
+  "B Major":  "o86XsPtpY5I",
+  "F Major":  "J7_KdfSWw7E",
+  "Bb Major": "GSoPb67MdTw",
+  "Eb Major": "KBlEe5qOt4I",
+  "Ab Major": "1WhynDu2B3A",
+  "Db Major": "iu2XHXz343U",
+}
 
 export const CorrectAnswer: FunctionComponent = () => {
   const {report} = useSelector(selectScaleEditor)
@@ -15,6 +30,7 @@ export const CorrectAnswer: FunctionComponent = () => {
   if(report && report.correct) {
     const scale = report?.scale
     if(scale) {
+      const youtubeVideoId = youtubeVideos[scale.name] || null
       let pitchClasses = scale.steps.map(p => parsePitch(p)?.midiNumber)
         .filter(p => typeof p === 'number')
         // @ts-ignore
@@ -28,6 +44,9 @@ export const CorrectAnswer: FunctionComponent = () => {
           highlightPitchClasses={pitchClasses}
           labelKeys="# or b"
         />
+        { youtubeVideoId 
+          ? <YouTube videoId={youtubeVideoId} /> 
+          : null}
       </div>
     } else
       throw new Error("Correct answer but no scale specified")
